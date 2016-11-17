48a70b4b35dc9e4f649df2cac7237e190a766c24
amnox <khalid_aman@ymail.com>
2016-11-17 07:36:52 +0530

    first commit with app ready for deployment

--- copyloftproject/settings.py
+++ copyloftproject/settings.py
@@ -51,6 +51,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+SESSION_ENGINE= 'django.contrib.sessions.backends.cached_db'
+
 ROOT_URLCONF = 'copyloftproject.urls'
 
 
