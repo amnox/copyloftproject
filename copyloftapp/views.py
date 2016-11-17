5683b0287360d527f608cfdedae29e2ce6f36f4a
amnox <khalid_aman@ymail.com>
2016-11-17 07:25:04 +0530

    first commit with app ready for deployment

--- copyloftapp/views.py
+++ copyloftapp/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 
 def index(request):
 #    return HttpResponse("Rango says hey there partner!")
-    if not request.session.exists(request.session.session_key):
-        request.session.create()
+#    if not request.session.exists(request.session.session_key):
+    request.session.create()
     print request.session.session_key
     return render(request, 'index.html')
 def upload(request):
