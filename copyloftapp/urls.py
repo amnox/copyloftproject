75aa8ccbd71e4eeb748a8cdc02a7d21c3180c172
rkc007 <rahulchauhan819@gmail.com>
2016-12-18 19:53:02 +0530

    here you are model and url

--- copyloftapp/urls.py
+++ copyloftapp/urls.py
@@ -1,7 +1,13 @@
 from copyloftapp import views  # @UnresolvedImport
-from django.conf.urls import url
+from django.conf.urls import  url
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^signup/$', views.signup, name='signup'),
     url(r'^createitem/upload/$', views.upload, name='create'),
+    url(r'^signup/signupcheck$', views.aftersignup, name='signupcheck'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^login/logincheck$', views.logincheck, name='logincheck'),
+    url(r'^signup/ajax/validate_username/$', views.validate_username, name='validate_username'),
+    url(r'^trial/$', views.example, name='trial'),
 ]
