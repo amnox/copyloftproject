264606f2980922579a9b04b57dec046060d0665c
rkc007 <rahulchauhan819@gmail.com>
2016-12-19 04:35:14 +0530

    KUCH BHI

--- copyloftapp/urls.py
+++ copyloftapp/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import  url
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-<<<<<<< HEAD
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^createitem/upload/$', views.upload, name='create'),
     url(r'^signup/signupcheck$', views.aftersignup, name='signupcheck'),
@@ -11,9 +10,7 @@ urlpatterns = [
     url(r'^login/logincheck$', views.logincheck, name='logincheck'),
     url(r'^signup/ajax/validate_username/$', views.validate_username, name='validate_username'),
     url(r'^trial/$', views.example, name='trial'),
-=======
     url(r'^createitem/$', views.cart_creation, name='create'),
     url(r'^createitem/upload/$', views.upload, name='fileupload'),
     url(r'^createitem/upload/sign_s3/(.*?)/(.*)$', views.sign_s3, name='signatureS3'),
->>>>>>> refs/remotes/amnox/master
 ]
